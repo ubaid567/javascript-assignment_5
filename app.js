612818c8afe995632a8e3894ca4936b612c1678a
ubaid567 <124505833+ubaid567@users.noreply.github.com>
2023-02-14 04:37:34 -0800

    chapter !4

---
+++ app.js
@@ -0,0 +1,152 @@
+// chapter  nama "arrays"
+//chapter # "13-15"
+
+
+//question #1
+//var studentNames = [];
+
+//question#2
+//var studentNames=newArrays();
+
+
+//question#3
+//var fruitNames=["apple","mango","banana","grapes","orange"];
+
+
+//question#4
+//var numberArays=[2004,2005,2006,2007,2008,2009,2010];
+//console.log(numberArays)
+
+
+
+//question#5
+//var booleanArray = [true, false, true, false];
+//console.log(booleanArray)
+
+
+//question#6
+//var mixedarray=["23","cat","true","null",[1,2,3]]
+//console .log(mixedarray)
+
+
+//question#7
+/*var qualification=["Ssc","Hsc","Bsc","Bs","Bcom","Ms","Phd","M Pihl"];
+document.write("<h1>Qualifications</h1>")
+document.write("<h2>1) "+ qualification[0]+"</h2>")
+document.write("<h2>2) "+ qualification[1]+"</h2>")
+document.write("<h2>3) "+ qualification[2]+"</h2>")
+document.write("<h2>4) "+ qualification[3]+"</h2>")
+document.write("<h2>5) "+ qualification[4]+"</h2>")
+document.write("<h2>6) "+ qualification[5]+"</h2>")
+document.write("<h2>7) "+ qualification[6]+"</h2>")
+document.write("<h2>8) "+ qualification[7]+"</h2>")*/
+
+
+//question#8
+
+/*var studentNames=["shayan","sheriyar","shaveer"]
+var studentMarks=["433","333","478"]
+document.write("<h1>Score of "+studentNames[0]+". Percentage:"+(studentMarks[0]*100)/500 +"%"+"</h1>")
+document.write("<h1>Score of "+studentNames[1]+". Percentage:"+(studentMarks[1]*100)/500 +"%"+"</h1>")
+document.write("<h1>Score of "+studentNames[2]+". Percentage:"+(studentMarks[2]*100)/500 +"%"+"</h1>")*/
+
+
+//question#9
+
+/*var color=["red","green","blue"]
+document.write("<p>" +"original array :"+ color+"</p>")
+
+var color1=prompt("enter the color who will add in start of array")
+color.unshift(color1)
+document.write("<p>" +" array after adding color one :"+ color+"</p>")
+
+
+
+var color2=prompt("enter the color who will add at the end of array")
+color.push(color2)
+document.write("<p>" +" array after adding color two :"+ color+"</p>")
+
+ color.unshift("brown","black")
+ document.write("<p>" +" array after adding two more colors :"+ color+"</p>")
+
+ color[0]=["pink"];
+ document.write("<p>" +" array after updating first value :"+ color+"</p>")
+
+ color.pop()
+ document.write("<p>" +" array after deleting first value :"+ color+"</p>")
+
+ var index=prompt("enter the number where you want to add a color 3")
+ var color3=prompt('entre color 3')
+ color.splice(index,0,color3)
+ document.write("<p>" +" array after adding color 3  :"+ color+"</p>")
+
+ var delIndex=prompt("Enter the index from where you want to delete colors")
+ var delcount=prompt("Enter the number of colors you want to delete:")
+ color.splice(delIndex,delcount)
+ document.write("<p>" +" array after deleting   :"+ color+"</p>")*/
+
+
+ //question#10
+
+//  var scores=["89","75","29","68","88","47"]
+//  document.write("score of student : "+scores+"<br/>")
+//  scores.sort()
+//  document.write("ordered scores of student"+scores)
+
+
+//question#11
+
+// var cities=["karachi","lahore","islmabad","faisalabad","multan"]
+// console.log(cities+"<br/>")
+// var selectedCites=cities.slice(2,4);
+// console.log(selectedCites)
+
+//question#12
+/* var arrays=["this ","is ","my ","cat"]
+ console.log(arrays)
+ var string=arrays.join("")
+ console.log(string)*/
+
+ //QUESTION#13
+/* var union=[]
+ union.push("owais");
+ union.push("rafay"); 
+ union.push("ubaid");
+ console.log(union) 
+
+ console.log(union.shift());
+ console.log(union.shift());
+ console.log(union.shift());*/
+
+ //question#14
+
+ /*var devices=[]
+ devices.push("moniter");
+ devices.push("cpu");
+ devices.push("keyboard");
+ devices.push("mouse");
+ console.log(devices);
+
+ console.log(devices.pop());
+ console.log(devices.pop());
+ console.log(devices.pop());
+ console.log(devices.pop());*/
+
+ //question#15
+/*var mobile=["sumsung","tecno","oppo","infinx","motorola","itel","apple","vivo","nokia","q mobile"]
+document.write("<select>")
+document.write("<option>"+mobile[0]+"</option>")
+document.write("<option>"+mobile[1]+"</option>")
+document.write("<option>"+mobile[2]+"</option>")
+document.write("<option>"+mobile[3]+"</option>")
+document.write("<option>"+mobile[4]+"</option>")
+document.write("<option>"+mobile[5]+"</option>")
+document.write("<option>"+mobile[6]+"</option>")
+document.write("<option>"+mobile[7]+"</option>")
+document.write("<option>"+mobile[8]+"</option>")
+document.write("<option>"+mobile[9]+"</option>")
+document.write("</select>")*/
+
+
+
+
